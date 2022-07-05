66007f6204f842702abe6f2c4157462179a2ddc7
Reaperbruhsans <107920979+Reaperbruhsans@users.noreply.github.com>
2022-07-05 22:56:13 +0300

    Update Numbers.c

--- Numbers.c
+++ Numbers.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 
 int main() {
+   int v1, v2, v3;
 
-int v1, v2, v3;
-
-for (int v1=0; v1<3; v1++)
-
-{
-
-for (int v2=0; v2<4; v2++)
-
-{
-
-for (int v3=0; v3<5; v3++)
-
-{
-
-int v1
-
-printf("%d, %d\n, %d\n",v1,v2, v³);
-
-}
-
-}
-
-}
-
+   for (int v1=0; v1<3; v1++)
+ {
+    for (int v2=0; v2<4; v2++)
+    {
+        for (int v3=0; v3<5; v3++)
+       {
+           {
+           printf("%d, %d\n, %d\n",v1,v2,v3);
+           }
+       }
+     }  
+  }
 int TotalAmount (v1 + v2 + v3);
-
 if (TotalAmount < 15) printf("There is not enough money.");
-
 else if (TotalAmount > 30) printf("You've got it just enough.");
 
 return 0;
